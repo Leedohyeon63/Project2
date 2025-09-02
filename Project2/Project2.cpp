e69f814141c106c8ecd243443838e87d47e20e7a
KGA <KGA@DESKTOP-CTTLHR7>
2025-09-02 11:52:32 +0900

    초기 커밋.

---
+++ Project2/Project2.cpp
@@ -0,0 +1,24 @@
+﻿//주석 : 프로그램에 영향을 끼치지 않는 부분
+//ctrl + d = 한줄 복사하기
+// shift + delete = 한줄 삭제하기 
+// shift + 화살표 = 블럭 잡기
+// alt + 화살표 = 라인 바꾸기
+// alt + shift + 화살표 = 블럭 잡기
+//ctrl + B + R = 프로젝트 리빌드
+#include <iostream>
+#include<stdio.h>
+#include<cstdio>
+#include "header.h"
+
+
+int main() { //엔트리 포인트(코드가 시작되는 곳)
+	//cout << "Hello world\n";
+	int number = 0;
+	printf("나이 : ");
+	cin >> number;
+	printf("%d세", number);
+
+	return 0;
+
+}
+
