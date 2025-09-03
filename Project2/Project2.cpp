1ff7da3e0a4017531998020c417e8771d5ec7e36
Leedohyeon63 <dhanrfk27@gmail.com>
2025-09-03 14:03:24 +0900

    if swich

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -9,48 +9,52 @@
 #include<stdio.h>
 #include<cstdio>
 #include "header.h"
-/* -3을 2진수로 표현하는 방법 
-* 3을 2진수로 바꾸면 0000 0011
-* 1. 1의 보수 만들기 1111 1100
-* 2. 2의 보수 만들기 1111 1101
-*  (1번에 1 더하면 됨)
-*
-
-
-
-*/ 
-
 
 int main() { //엔트리 포인트(코드가 시작되는 곳)
-	 
-	//float n1 = 10.0f;
-	//float n2 = 15.0;
-	//cin >> n1 >> n2;
-	//printf("n1 : %f \nn2 : %.2f", n1, n2);
-
-	//float PI = 3.14f, R = 0.0f;
-	//cin >> R;
-	//printf("넓이 : %f", PI * R * R);
-
-	//int s = 0;
-	//cin >> s;
-	//printf("넓이 : %d", s*s);
-
-	float a = 123.0f;
-	float b = 0.0f;
+	int a = 10;
+	int b = 0;
 	cin >> b;
+	if (b < 60)
+	{
+		printf("f");
+	}
+	else if (b < 70)
+	{
+		printf("d");
+	}
+	else if (b < 80)
+	{
+		printf("c");
+	}
+	else if (b < 90)
+	{
+		printf("b");
+	}
+	else
+	{
+		printf("a");
+	}
 
-	bool result = a==b;
-
-	//float 타입을 ==로 비교할 때 반드시 아래와 같이 진행해야 함
-	//float epsilon = 0.001f;
-	//a < (b + epsilon);
-	//b > (a + epsilon);
+	cin >> a;
+	switch (a)
+	{
+	case 1:
+		printf("1");
+		break;
+	case 3:
+		printf("3");
+		break;
+	case 5:
+		printf("5");
+		break;
+	default:
+		printf("x");
+		break;
+	}
 
 
 
 
 	return 0;
-
 }
 
