1b3e47a8512da021795302230177cee79f3bfb88
Leedohyeon63 <dhanrfk27@gmail.com>
2025-09-05 16:28:58 +0900

    실습수정+주석최종

--- Project2/Practice.cpp
+++ Project2/Practice.cpp
@@ -1,7 +1,7 @@
 #include "Practice.h"
 #include "header.h"
 
-int Randreturn(int Rnum1, int Rnum2)
+int Randreturn(int Rnum1, int Rnum2)//1번문제 두 값 사이를 랜덤값 출력
 {
 	int R = rand() % Rnum2;
 	if (R < Rnum1)
@@ -14,18 +14,18 @@ int Randreturn(int Rnum1, int Rnum2)
 	}
 }
 
-void ageret(int Age)
+void ageret(int Age)//2번문제 나이 출력
 {
 	printf("\n당신의 나이는 %d살 입니다.", Age);
 }
 
-float Round(float Round)
+float Round(float Round)//3번문제 반지름 받아 원 넓이 출력
 {
 	float PI = 3.14f;
 	return Round * Round * PI;
 }
 
-bool Holzzak(int Num)
+bool Holzzak(int Num)//4번문제 홀수인지 짝수인지 구해서 t/f출력
 {
 	if (Num % 2 == 0)
 	{
@@ -37,7 +37,7 @@ bool Holzzak(int Num)
 	}
 }
 
-int Grade(int Grade)
+int Grade(int Grade)//5번문제 성적따라 다른값 리턴
 {
 	enum Greadscale
 	{
@@ -71,22 +71,22 @@ int Grade(int Grade)
 	return 0;
 }
 
-int invenPuls(int inventory, int Bflag)
+int invenPuls(int inventory, int Bflag)//6번문제 인벤토리 비트값 or해서 아이템 추가 하는 함수
 {
 	return inventory | Bflag;
 }
 
-int invenRemove(int inventory, int Bflag)
+int invenRemove(int inventory, int Bflag)//6번문제 인벤토리 비트값 and해서 아이템 추가 하는 함수
 {
 	return inventory & ~Bflag;
 }
 
-int invenSwitch(int inventory, int Bflag)
+int invenSwitch(int inventory, int Bflag)//6번문제 인벤토리 비트값 xor해서 아이템 토글로 온오프하는 함수
 {
 	return inventory ^ Bflag;
 }
 
-void Primide(int p)
+void Primide(int p)//7번문제 피라미드 출력하는 함수
 {
 	int P = p;
 	for (int i = 0; i < P; i++)
--- Project2/Practice.h
+++ Project2/Practice.h
@@ -8,15 +8,29 @@ int invenPuls(int inventory, int Bflag);
 int invenRemove(int inventory, int Bflag);
 int invenSwitch(int Inventory, int Bflag);
 void Primide(int p);
-template <typename T>
+template <typename T> //min출력
 T Min(T num1, T num2) {
-	return min(num1,num2);
+	if (num1 < num2)
+	{
+		return num1;
+	}
+	else
+	{
+		return num2;
+	}
 }
-template <typename T>
+template <typename T>//max출력
 T Max(T num1, T num2) {
-	return max(num1, num2);
+	if (num1 > num2)
+	{
+		return num1;
+	}
+	else
+	{
+		return num2;
+	}
 }
-template <typename T>
+template <typename T>//중간값 출력
 T Clamp(T min, T max, T value) {
 	if(value - min < 0)
 	{
@@ -31,7 +45,7 @@ T Clamp(T min, T max, T value) {
 		return value;
 	}
 }
-template <typename T>
+template <typename T>//팩토리얼
 T facto(T num1) {
 	while (num1 <= 1)
 	{
