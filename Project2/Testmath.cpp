56c34fd153d29cd76f42088e51ae62d62082218d
Leedohyeon63 <dhanrfk27@gmail.com>
2025-09-05 12:00:43 +0900

    func

---
+++ Project2/Testmath.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Testmath.h"
+using namespace std;
+
+int sum(int a, int b)
+{
+	return a+b;
+}
+
+void printHelloworld()
+{
+	printf("");
+}
+
+int multiPly(int num1, int num2)
+{
+	return num1*num2;
+}
---
+++ Project2/Testmath.h
@@ -0,0 +1,4 @@
+#pragma once
+int sum(int a, int b);// 두 수를 더해서 리턴하는 함수
+void printHelloworld();// 헬로월드를 출력하는 함수
+int multiPly(int num1, int num2=2);// 두 수의 곱을 리턴하는 함수
